6ac383b4c87b084df1ed823e2cea43ee338e7e85
Anthony Heinrichs <anthony.g.heinrichs@gmail.com>
2022-11-11 18:24:58 +0100

    Added new method find, to pass back the index of the passed value if it exists

--- script.js
+++ script.js
@@ -1,99 +1,139 @@
 const Node = (data = null, next = null) => {
-  return {data, next}
-}
+  return { data, next };
+};
 const LinkedList = () => {
-  let head = null
-  let tail = null
-  let size = 0
+  let head = null;
+  let tail = null;
+  let size = 0;
 
   // Adding new node to head of linked list
   const append = (value) => {
-    let newNode = Node(value)
+    let newNode = Node(value);
 
     if (!head) {
-      head = newNode
-      tail = newNode
-      size++
-      return {head, tail}
+      head = newNode;
+      tail = newNode;
+      size++;
+      return { head, tail };
     }
 
-    let current = head
+    let current = head;
     while (current.next) {
-      current = current.next
+      current = current.next;
     }
-    current.next = newNode
-    tail = newNode
-    size++
-    return tail
-  }
+    current.next = newNode;
+    tail = newNode;
+    size++;
+    return tail;
+  };
 
   // Adding new node to tail of linked list
   const prepend = (value) => {
-    let newNode = Node(value, head)
-    head = newNode
-    size++
-    return head
-  }
+    let newNode = Node(value, head);
+    head = newNode;
+    size++;
+    return head;
+  };
 
   // Finding the node at a specific index
   const at = (index) => {
-    let current = head
+    let current = head;
     for (let i = 0; i < index; i++) {
-      current = current.next
+      current = current.next;
       if (!current) {
-        return 'There is no node at that index'
+        return "There is no node at that index";
       }
     }
-    return current
-  } 
+    return current;
+  };
 
   // Finding the last node of the linked list, and removing it from list
   const pop = () => {
-    let current = head
-    let prevNode = null
+    let current = head;
+    let prevNode = null;
     while (current.next) {
-      prevNode = current
-      current = current.next
+      prevNode = current;
+      current = current.next;
     }
-    prevNode.next = null
-  }
+    prevNode.next = null;
+  };
 
   // Checking if a node in the linked list contains the passed value
   const contains = (value) => {
-    let current = head
+    let current = head;
+
+    while (current.next) {
+      current = current.next;
+      if (current.data == value) {
+        return true;
+      }
+    }
+    if (current.data == value) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const find = (value) => {
+    let current = head;
+    let index = 0;
+
     while (current.next) {
       if (current.data == value) {
-        return true
+        return index;
       }
       current = current.next;
+      index++;
     }
-    return false
-  }
+    if (current.data == value) {
+      return index;
+    }
+
+    return null;
+  };
 
   // Converting linked list to string and returning string
   const toString = () => {
-    let string = ''
+    let string = "";
 
     if (!head) {
-      string = 'The linked list is empty'
-      return string
+      string = "The linked list is empty";
+      return string;
     }
-    
-    let current = head
+
+    let current = head;
     while (current !== null) {
       string += `( ${current.data} ) -> `;
       current = current.next;
     }
     return `${string}null`;
-  }
+  };
 
-  return {append, prepend, at, pop, contains, toString, get head() { return head }, get size() { return size }, get tail() {return tail}}
-}
+  return {
+    append,
+    prepend,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    get head() {
+      return head;
+    },
+    get size() {
+      return size;
+    },
+    get tail() {
+      return tail;
+    },
+  };
+};
 
-const newLinkedList = LinkedList()
+const newLinkedList = LinkedList();
 
-newLinkedList.append('John')
-newLinkedList.append('Tony')
-newLinkedList.append('Benji')
-newLinkedList.prepend('Taco')
-console.log(newLinkedList.contains('Tony'))
+newLinkedList.append("John");
+newLinkedList.append("Tony");
+newLinkedList.append("Benji");
+newLinkedList.prepend("Taco");
+console.log(newLinkedList.find("Taco"));
